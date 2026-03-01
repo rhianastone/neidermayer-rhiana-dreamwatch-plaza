cd2bd9e500cf5fed07cfe88f9bc493bdc605b44b
rhianastone <rneidermayer@usv.edu>
2026-02-28 23:37:54 -0800

    Collision and Material blockout
    replaced some of my old block out with updated models and scenes, organized project, added collision to all model assets. added a few omni lights

--- addons/proto_controller/proto_controller.gd
+++ addons/proto_controller/proto_controller.gd
@@ -22,7 +22,7 @@ extends CharacterBody3D
 ## Normal speed.
 @export var base_speed : float = 7.0
 ## Speed of jump.
-@export var jump_velocity : float = 4.5
+@export var jump_velocity : float = 8.5
 ## How fast do we run?
 @export var sprint_speed : float = 10.0
 ## How fast do we freefly?
